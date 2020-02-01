097af94470f1a69f19255c61f208595a65c849af
Chad Ogle <ecogle@gmail.com>
2020-02-01 06:33:19 -0600

    Added a configuration file

--- src/com/pgsql/DBTest.java
+++ src/com/pgsql/DBTest.java
@@ -6,21 +6,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import tools.Config;
+
 public class DBTest {
-	//static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-	//static final String JDBC_DRIVER = "org.postgresql.Driver";  
-	static final String DB_URL = "jdbc:postgresql://localhost:5432/TestDB";
-	
-	static final String USER = "ecogle";
-	static final String PASS = "F22R@ptor";
-	
-	public static void main (String[] args) throws SQLException {
-	
-		try(Connection conn = DriverManager.getConnection(DB_URL,USER,PASS)){
-			//Class .forName("com.mysql.jdbc.Driver");
 			
+	public static void main (String[] args) throws SQLException {
+		
+		Config config = new Config(Config.DB_TYPES.POSTGRES);
+	
+		try(Connection conn = DriverManager.getConnection(config.getURL(),config.getUsername(),config.getPassword())){
+						
 			Statement stmt = conn.createStatement();
-			//String sel = "SELECT id, first_name,last_name,age FROM Person";
+			
 			String sel = "SELECT site_id, name FROM sites";
 			
 			try(ResultSet rs = stmt.executeQuery(sel)){
@@ -29,7 +26,7 @@ public class DBTest {
 				}
 			}
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 	}
---
+++ src/tools/Config.java
@@ -0,0 +1,114 @@
+package tools;
+
+import java.io.File;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class Config {
+	
+	private String host;
+	private String port;
+	private String username;
+	private String password;
+	private String database;
+	DB_TYPES type;
+	
+	public Config(DB_TYPES dbtype) {
+		this.type = dbtype;
+		initialize(dbtype.getName());
+		
+		
+	}
+	
+	private void initialize(String dbtype) {
+		File file = new File("configuration.xml");
+		
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dbBuilder;
+		try {
+			dbBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dbBuilder.parse(file);
+			
+			doc.getDocumentElement().normalize();
+			
+			NodeList nodes = doc.getElementsByTagName(dbtype);
+			
+			for(int x = 0; x < nodes.getLength(); x ++)
+			{
+				Node node = nodes.item(x);
+				
+				
+				
+				
+				System.out.println(node.getNodeName());
+				if(node.getNodeType() == Node.ELEMENT_NODE) {
+					Element element = (Element) node;
+					this.host = (element.getElementsByTagName("host").item(0).getTextContent());
+					this.port = (element.getElementsByTagName("port").item(0).getTextContent());
+					this.username = (element.getElementsByTagName("username").item(0).getTextContent());
+					this.password = (element.getElementsByTagName("password").item(0).getTextContent());
+					this.database = (element.getElementsByTagName("database").item(0).getTextContent());
+				}
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static enum DB_TYPES{
+		POSTGRES("postgresql"),MYSQL("mysql");
+		private String name;
+		
+		DB_TYPES(String name) {
+			this.name = name;
+		}
+		
+		private String getName() {
+			return this.name;
+		}
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public String getPort() {
+		return port;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+	
+	public String getDatabase() {
+		return database;
+	}
+
+	public String getURL() {
+		
+		StringBuilder url = new StringBuilder();
+		url.append("jdbc:");
+		url.append(this.type.getName());
+		url.append("://").append(this.host).append(":").append(this.port).append("/").append(this.database);
+		
+		return url.toString();
+	}
+	
+	
+	
+
+}
